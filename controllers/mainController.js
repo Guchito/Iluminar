a7fdc5ee04844c8eca6c0796a4a43ba9b0babbd1
Guchito <a_gaggero@hotmail.com>
2023-02-15 13:44:34 +0100

    Fixed routes for products

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,13 +1,10 @@
-const controller = {
+const mainController = {
     index: (req, res) => {
         res.render('index');
     },
     error: (req, res) => {
         res.render('error');
-    },
-    lamps: (req, res) => {
-        res.render('lamps');
     }
 }
 
-module.exports = controller;
+module.exports = mainController;
