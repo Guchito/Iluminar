056cec7dfd6b304dec993dc23a2bfa5d9972f10c
Guchito <a_gaggero@hotmail.com>
2023-02-22 15:04:36 +0100

    Change visually discount box

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,9 @@
 new Swiper("#swiper-1", {
     effect: "fade",
-    /*autoplay: {
+    autoplay: {
         delay: 3500,
         disableOnInteraction: false,
-    },*/
+    },
     pagination: {
         el: "#swiper-1 .swiper-pagination",
         clickable: true,
