24379cdcf0a800d9bfbee5e7b9abea6f62f4a53a
Guchito <a_gaggero@hotmail.com>
2023-02-24 15:54:48 +0100

    Hamburger menu, not working yet

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,25 @@
-new Swiper("#swiper-1", {
-    effect: "fade",
-    autoplay: {
-        delay: 3500,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: "#swiper-1 .swiper-pagination",
-        clickable: true,
-    },
-    lazyLoading: true,
-    loop: true
-});
+window.onload = function () {
+    new Swiper("#swiper-1", {
+        effect: "fade",
+        autoplay: {
+            delay: 3500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: "#swiper-1 .swiper-pagination",
+            clickable: true,
+        },
+        lazyLoading: true,
+        loop: true
+    });
+
+    const mobile_menu = document.querySelector('.mobile-nav');
+    const menu_btn = document.querySelector('.hamburger');
+    menu_btn.addEventListener('click', function() {
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    })
+
+
+}
+
